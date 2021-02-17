2b58920c0f366e51bd757a2d00be5c462360b497
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-02-17 16:29:13 -0500

    added button functionlity

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ const compDisplay = document.querySelector('#compDisplay')
 const results = document.querySelector('#results')
 let userScore = 0;
 let compScore = 0;
-let winningScore = 0;
+let winningScore = 5;
 
 
 const btnRock = document.querySelector('#btn-rock')
 btnRock.addEventListener('click', () => {
     results.textContent = playOneRound("rock", computerPlay())
+    userDisplay.textContent = userScore;
+    compDisplay.textContent = compScore;
+    if (userScore === winningScore || compScore === winningScore) {
+        results.textContent = "The End"
+    }
 })
 
 
@@ -18,15 +23,30 @@ btnRock.addEventListener('click', () => {
 
 const btnPaper = document.querySelector('#btn-paper')
 btnPaper.addEventListener('click', () => {
-    alert('Paper')
+    results.textContent = playOneRound("rock", computerPlay())
+    userDisplay.textContent = userScore;
+    compDisplay.textContent = compScore;
+    if (userScore === winningScore || compScore === winningScore) {
+        results.textContent = "The End"
+    }
 })
 const btnScissor = document.querySelector('#btn-scissor')
 btnScissor.addEventListener('click', () => {
-    alert('Scissor')
+    results.textContent = playOneRound("rock", computerPlay())
+    userDisplay.textContent = userScore;
+    compDisplay.textContent = compScore;
+    if (userScore === winningScore || compScore === winningScore) {
+        results.textContent = "The End"
+    }
 })
 
-// userDisplay.textContent = userScore;
-
+const reset = document.querySelector('#reset')
+reset.addEventListener('click', () => {
+    userScore = 0;
+    compScore = 0;
+    userDisplay.textContent = 0;
+    compDisplay.textContent = 0;
+})
 function computerPlay() {
     const rand = Math.floor(Math.random() * 3);
     if (rand === 0) {
@@ -41,21 +61,27 @@ function computerPlay() {
 
 function playOneRound(playerSelection, computerSelection) {
     if (playerSelection === 'paper' && computerSelection === 'scissor') {
+        compScore += 1;
         return "You Lose! Scissor beats Paper"
     }
     else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        userScore += 1;
         return "You Win! Paper beats Rock"
     }
     else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+        compScore += 1;
         return "You Lose! Rock beats Scissor"
     }
     else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+        userScore += 1;
         return "You Win! Scissor beats Paper"
     }
     else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        compScore += 1;
         return "You Lose! Paper beats Rock"
     }
     else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+        userScore += 1;
         return "You Win! Rock beats Scissor"
     }
     else {
