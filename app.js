c0e0ae6ca43838a852c42e91a4545ce0fcd696ce
Jimmy Zhang <Jzjimmy3@gmail.com>
2021-02-27 23:37:50 -0500

    Elaborated with code logic

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 
-// // function for making a computer play Rock-Paper-Scissors
+// // function for making a computer play Sword-Shield-Scissors
 const userDisplay = document.querySelector('#userDisplay')
 const compDisplay = document.querySelector('#compDisplay')
 const results = document.querySelector('#results')
+let compStorage;
 let userScore = 0;
 let compScore = 0;
 let winningScore = 5;
 const gameStart = true;
 
 
-const btnRock = document.querySelector('#btn-rock')
-btnRock.addEventListener('click', () => {
+const btnSword = document.querySelector('#btn-sword')
+btnSword.addEventListener('click', () => {
     if (gameStart) {
-        results.textContent = playOneRound("rock", computerPlay())
+        results.textContent = playOneRound("sword", computerPlay())
         userDisplay.textContent = userScore;
         compDisplay.textContent = compScore;
+        commentate.textContent = "You chose Sword Opponent chose: " + compStorage
     }
     if (userScore === winningScore || compScore === winningScore) {
         gameOver();
@@ -22,24 +24,27 @@ btnRock.addEventListener('click', () => {
     }
 })
 
-const btnPaper = document.querySelector('#btn-paper')
-btnPaper.addEventListener('click', () => {
+const btnShield = document.querySelector('#btn-shield')
+btnShield.addEventListener('click', () => {
     if (gameStart) {
-        results.textContent = playOneRound("rock", computerPlay())
+        results.textContent = playOneRound("shield", computerPlay())
         userDisplay.textContent = userScore;
         compDisplay.textContent = compScore;
+        commentate.textContent = "You chose Shield Opponent chose: " + compStorage
     }
     if (userScore === winningScore || compScore === winningScore) {
         gameOver()
         results.textContent = "The End"
     }
 })
-const btnScissor = document.querySelector('#btn-scissor')
-btnScissor.addEventListener('click', () => {
+
+const btnSpear = document.querySelector('#btn-spear')
+btnSpear.addEventListener('click', () => {
     if (gameStart) {
-        results.textContent = playOneRound("rock", computerPlay())
+        results.textContent = playOneRound("spear", computerPlay())
         userDisplay.textContent = userScore;
         compDisplay.textContent = compScore;
+        commentate.textContent = "You chose Spear Opponent chose: " + compStorage
     }
     if (userScore === winningScore || compScore === winningScore) {
         gameOver()
@@ -53,55 +58,64 @@ reset.addEventListener('click', () => {
     compScore = 0;
     userDisplay.textContent = 0;
     compDisplay.textContent = 0;
-    document.getElementById('btn-rock').disabled = false;
-    document.getElementById('btn-paper').disabled = false;
-    document.getElementById('btn-scissor').disabled = false;
+    document.getElementById('btn-sword').disabled = false;
+    document.getElementById('btn-shield').disabled = false;
+    document.getElementById('btn-spear').disabled = false;
+    commentate.textContent = "";
+    results.textContent = "";
 })
 function computerPlay() {
     const rand = Math.floor(Math.random() * 3);
     if (rand === 0) {
-        return 'rock'
+        return 'sword'
     }
     else if (rand === 1) {
-        return 'paper'
+        return 'shield'
     } else {
-        return 'scissor'
+        return 'spear'
     }
 }
 
 function gameOver() {
-    document.getElementById('btn-rock').disabled = true;
-    document.getElementById('btn-paper').disabled = true;
-    document.getElementById('btn-scissor').disabled = true;
+    document.getElementById('btn-sword').disabled = true;
+    document.getElementById('btn-shield').disabled = true;
+    document.getElementById('btn-spear').disabled = true;
     gameStart = false;
 }
 
 function playOneRound(playerSelection, computerSelection) {
-    if (playerSelection === 'paper' && computerSelection === 'scissor') {
+    if (playerSelection === 'shield' && computerSelection === 'spear') {
         compScore += 1;
-        return "You Lose! Scissor beats Paper"
+        compStorage = "Spear";
+        return "You Lose! Spear beats Shield"
     }
-    else if (playerSelection === 'paper' && computerSelection === 'rock') {
+    else if (playerSelection === 'shield' && computerSelection === 'sword') {
         userScore += 1;
-        return "You Win! Paper beats Rock"
+        compStorage = "Sword";
+        return "You Win! Shield beats Sword"
     }
-    else if (playerSelection === 'scissor' && computerSelection === 'rock') {
+    else if (playerSelection === 'spear' && computerSelection === 'sword') {
         compScore += 1;
-        return "You Lose! Rock beats Scissor"
+        compStorage = "Sword";
+        return "You Lose! Sword beats Spear"
     }
-    else if (playerSelection === 'scissor' && computerSelection === 'paper') {
+    else if (playerSelection === 'spear' && computerSelection === 'shield') {
         userScore += 1;
-        return "You Win! Scissor beats Paper"
+        compStorage = "Shield";
+        return "You Win! Spear beats Shield"
     }
-    else if (playerSelection === 'rock' && computerSelection === 'paper') {
+    else if (playerSelection === 'sword' && computerSelection === 'shield') {
         compScore += 1;
-        return "You Lose! Paper beats Rock"
+        compStorage = "Shield"
+        return "You Lose! Shield beats Sword"
     }
-    else if (playerSelection === 'rock' && computerSelection === 'scissor') {
+    else if (playerSelection === 'sword' && computerSelection === 'spear') {
         userScore += 1;
-        return "You Win! Rock beats Scissor"
+        compStorage = "Spear"
+        return "You Win! Sword beats Spear"
     }
     else {
+        compStorage = playerSelection
         return "Tie!"
     }
 }
